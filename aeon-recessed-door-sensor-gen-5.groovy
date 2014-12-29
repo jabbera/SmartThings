25e4626dd44f6847366b5a14ce6076f447672c94
Mike <jabbera@users.noreply.github.com>
2014-12-29 00:05:09 -0500

    aeon recessed sensor, make battery work.

--- aeon-recessed-door-sensor-gen-5.groovy
+++ aeon-recessed-door-sensor-gen-5.groovy
@@ -7,7 +7,7 @@
  *
  *  Author: Mike
  *  Modified by: Mike
- *  Date: 2014-12-16
+ *  Date: 2014-12-28
  */
 
 // for the UI
@@ -18,7 +18,7 @@ metadata {
 		capability "Sensor"
 		capability "Battery"
 
-		fingerprint deviceId: "0x2001", inClusters: "0x30,0x80,0x84,0x70,0x85,0x86,0x72"
+		fingerprint deviceId: "0x0701", inClusters: "0x5E,0x86,0x72,0x98,0xEF,0x5A,0x82"
 	}
 
 	// simulator metadata
@@ -46,13 +46,15 @@ metadata {
 
 def parse(String description) {
 	def result = null
-	if (description.startsWith("Err")) {
+    
+    if (description.startsWith("Err")) {
 	    result = createEvent(descriptionText:description)
 	} else if (description == "updated") {
 		if (!state.MSR) {
+        	log.debug("NO MSR")
 			result = [
-				response(zwave.wakeUpV1.wakeUpIntervalSet(seconds:4*3600, nodeid:zwaveHubNodeId)),
-				response(zwave.manufacturerSpecificV2.manufacturerSpecificGet()),
+				secure(zwave.wakeUpV1.wakeUpIntervalSet(seconds:8*60*60, nodeid:zwaveHubNodeId)),
+				secure(zwave.manufacturerSpecificV2.manufacturerSpecificGet()),
 			]
 		}
 	} else {
@@ -60,7 +62,10 @@ def parse(String description) {
 		if (cmd) {
 			result = zwaveEvent(cmd)
 		}
-	}
+    }
+
+	// log.debug("Parse returned ${result}")
+
 	return result
 }
 
@@ -82,76 +87,20 @@ def sensorValueEvent(value) {
 	}
 }
 
-def zwaveEvent(physicalgraph.zwave.commands.basicv1.BasicReport cmd)
-{
-	sensorValueEvent(cmd.value)
-}
-
 def zwaveEvent(physicalgraph.zwave.commands.basicv1.BasicSet cmd)
 {
 	sensorValueEvent(cmd.value)
 }
 
-def zwaveEvent(physicalgraph.zwave.commands.switchbinaryv1.SwitchBinaryReport cmd)
-{
-	sensorValueEvent(cmd.value)
-}
-
-def zwaveEvent(physicalgraph.zwave.commands.sensorbinaryv1.SensorBinaryReport cmd)
-{
-	sensorValueEvent(cmd.sensorValue)
-}
-
-def zwaveEvent(physicalgraph.zwave.commands.sensoralarmv1.SensorAlarmReport cmd)
-{
-	sensorValueEvent(cmd.sensorState)
-}
-
-def zwaveEvent(physicalgraph.zwave.commands.notificationv3.NotificationReport cmd)
-{
-	def result = []
-	if (cmd.notificationType == 0x06 && cmd.event == 0x16) {
-		result << sensorValueEvent(1)
-	} else if (cmd.notificationType == 0x06 && cmd.event == 0x17) {
-		result << sensorValueEvent(0)
-	} else if (cmd.notificationType == 0x07) {
-		if (cmd.v1AlarmType == 0x07) {  // special case for nonstandard messages from Monoprice door/window sensors
-			result << sensorValueEvent(cmd.v1AlarmLevel)
-		} else if (cmd.event == 0x01 || cmd.event == 0x02) {
-			result << sensorValueEvent(1)
-		} else if (cmd.event == 0x03) {
-			result << createEvent(descriptionText: "$device.displayName covering was removed", isStateChange: true)
-			result << response(zwave.wakeUpV1.wakeUpIntervalSet(seconds:4*3600, nodeid:zwaveHubNodeId))
-			if(!state.MSR) result << response(zwave.manufacturerSpecificV2.manufacturerSpecificGet())
-		} else if (cmd.event == 0x05 || cmd.event == 0x06) {
-			result << createEvent(descriptionText: "$device.displayName detected glass breakage", isStateChange: true)
-		} else if (cmd.event == 0x07) {
-			if(!state.MSR) result << response(zwave.manufacturerSpecificV2.manufacturerSpecificGet())
-			result << createEvent(name: "motion", value: "active", descriptionText:"$device.displayName detected motion")
-		}
-	} else if (cmd.notificationType) {
-		def text = "Notification $cmd.notificationType: event ${([cmd.event] + cmd.eventParameter).join(", ")}"
-		result << createEvent(name: "notification$cmd.notificationType", value: "$cmd.event", descriptionText: text, displayed: false)
-	} else {
-		def value = cmd.v1AlarmLevel == 255 ? "active" : cmd.v1AlarmLevel ?: "inactive"
-		result << createEvent(name: "alarm $cmd.v1AlarmType", value: value, displayed: false)
-	}
-	result
-}
-
 def zwaveEvent(physicalgraph.zwave.commands.wakeupv1.WakeUpNotification cmd)
 {
 	def result = [createEvent(descriptionText: "${device.displayName} woke up", isStateChange: false)]
-	if (!state.lastbat || (new Date().time) - state.lastbat > 53*60*60*1000) {
-		result << response(zwave.batteryV1.batteryGet())
+    
+    if (!state.lastbat || (new Date().time) - state.lastbat > 12*60*60*1000) {
+		result << secure(zwave.batteryV1.batteryGet())
 		result << response("delay 1200")
 	}
-	if (!state.MSR) {
-		result << response(zwave.wakeUpV1.wakeUpIntervalSet(seconds:4*3600, nodeid:zwaveHubNodeId))
-		result << response(zwave.manufacturerSpecificV2.manufacturerSpecificGet())
-		result << response("delay 1200")
-	}
-	result << response(zwave.wakeUpV1.wakeUpNoMoreInformation())
+	result << secure(zwave.wakeUpV1.wakeUpNoMoreInformation())
 	result
 }
 
@@ -165,7 +114,7 @@ def zwaveEvent(physicalgraph.zwave.commands.batteryv1.BatteryReport cmd) {
 		map.value = cmd.batteryLevel
 	}
 	state.lastbat = new Date().time
-	[createEvent(map), response(zwave.wakeUpV1.wakeUpNoMoreInformation())]
+	[createEvent(map), secure(zwave.wakeUpV1.wakeUpNoMoreInformation())]
 }
 
 def zwaveEvent(physicalgraph.zwave.commands.manufacturerspecificv2.ManufacturerSpecificReport cmd) {
@@ -175,14 +124,12 @@ def zwaveEvent(physicalgraph.zwave.commands.manufacturerspecificv2.ManufacturerS
 	log.debug "msr: $msr"
 	updateDataValue("MSR", msr)
 
-	retypeBasedOnMSR()
-
 	result << createEvent(descriptionText: "$device.displayName MSR: $msr", isStateChange: false)
 
 	if (msr == "011A-0601-0901") {  // Enerwave motion doesn't always get the associationSet that the hub sends on join
-		result << response(zwave.associationV1.associationSet(groupingIdentifier:1, nodeId:zwaveHubNodeId))
+		result << secure(zwave.associationV1.associationSet(groupingIdentifier:1, nodeId:zwaveHubNodeId))
 	} else if (!device.currentState("battery")) {
-		result << response(zwave.batteryV1.batteryGet())
+		result << secure(zwave.batteryV1.batteryGet())
 	}
 
 	result
@@ -192,20 +139,6 @@ def zwaveEvent(physicalgraph.zwave.Command cmd) {
 	createEvent(descriptionText: "$device.displayName: $cmd", displayed: false)
 }
 
-def retypeBasedOnMSR() {
-	switch (state.MSR) {
-		case "0086-0002-002D":
-			log.debug("Changing device type to Z-Wave Water Sensor")
-			setDeviceType("Z-Wave Water Sensor")
-			break
-		case "011F-0001-0001":  // Schlage motion
-		case "014A-0001-0001":  // Ecolink motion
-		case "0060-0001-0002":  // Everspring SP814
-		case "0060-0001-0003":  // Everspring HSP02
-		case "011A-0601-0901":  // Enerwave ZWN-BPC
-			log.debug("Changing device type to Z-Wave Motion Sensor")
-			setDeviceType("Z-Wave Motion Sensor")
-			break
-		
-	}
+private secure(physicalgraph.zwave.Command cmd) {
+	response(zwave.securityV1.securityMessageEncapsulation().encapsulate(cmd).format())
 }
