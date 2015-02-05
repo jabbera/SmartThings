66eb74d536b62f8e53f00f9149bd0f7f84e2dbc9
Mike <github@barry.io>
2015-02-04 21:38:25 -0500

    Update sensor to report every 12 hours. Log all debug and battery messages

--- aeon-recessed-door-sensor-gen-5.groovy
+++ aeon-recessed-door-sensor-gen-5.groovy
@@ -4,18 +4,18 @@
  *
  *  Author: Mike
  *  Modified by: Mike
- *  Date: 2014-12-28
+ *  Date: 2015-2-4
  */
 
 // for the UI
 metadata {
 	// Automatically generated. Make future change here.
-	definition (name: "Aeon Recessed Door Sensor", namespace: "jabbera", author: "Mike") {
+	definition (name: "BETA - Aeon Recessed Door Sensor", namespace: "jabbera", author: "Mike") {
 		capability "Contact Sensor"
 		capability "Sensor"
 		capability "Battery"
-
-		fingerprint deviceId: "0x0701", inClusters: "0x5E,0x86,0x72,0x98,0xEF,0x5A,0x82"
+        
+		fingerprint deviceId: "0x0705", inClusters: "0x5E,0x86,0x72,0x98,0xEF,0x5A,0x82"
 	}
 
 	// simulator metadata
@@ -34,33 +34,44 @@ metadata {
         	valueTile("battery", "device.battery", inactiveLabel: false, decoration: "flat") {
 			state "battery", label:'${currentValue}% battery', unit:""
 		}
-
+        standardTile("refresh", "device.power", inactiveLabel: false, decoration: "flat") {
+	    	state "default", label:'', action:"refresh.refresh", icon:"st.secondary.refresh"
+		}
 
 		main "contact"
-		details(["contact", "battery"])
+		details(["contact", "battery","configure"])
 	}
 }
+
+/*
+	Upon intial connection the switch sends an unsolicited ManufacturerSpecificReport
+    We will setup the wake up interval there. This is a sleepy device meaning you cannot
+    send it commands at any time. You can only send them between wake up intervals. This 
+    means the interval will only change once the device has woken up and we can ask it to change
+    it connot be less then 8 minutes.
+*/
+
 
 def parse(String description) {
 	def result = null
     
+    log.debug("Parse got ${description?.inspect()}")
+    
     if (description.startsWith("Err")) {
 	    result = createEvent(descriptionText:description)
 	} else if (description == "updated") {
-		if (!state.MSR) {
-        		result = [
-				secure(zwave.wakeUpV1.wakeUpIntervalSet(seconds:11*60*60 + 300, nodeid:zwaveHubNodeId)),
-				secure(zwave.manufacturerSpecificV2.manufacturerSpecificGet()),
-			]
-		}
+    	// nothing to do.
 	} else {
-		def cmd = zwave.parse(description, [0x20: 1, 0x25: 1, 0x30: 1, 0x31: 5, 0x80: 1, 0x84: 1, 0x71: 3, 0x9C: 1])
+		def cmd = zwave.parse(description, [0x20: 1, 0x25: 1, 0x30: 1, 0x31: 5, 0x80: 1, 0x84: 2, 0x71: 3, 0x9C: 1])
+        
+        if (state.debug) log.debug("Parsed command: ${cmd?.inspect()}")
+        
 		if (cmd) {
 			result = zwaveEvent(cmd)
 		}
-    	}
+   	}
 
-	// log.debug("Parse returned ${result}")
+ 	log.debug("Parse returned ${result}")
 
 	return result
 }
@@ -68,14 +79,22 @@ def parse(String description) {
 // Devices that support the Security command class can send messages in an encrypted form;
 // they arrive wrapped in a SecurityMessageEncapsulation command and must be unencapsulated
 def zwaveEvent(physicalgraph.zwave.commands.securityv1.SecurityMessageEncapsulation cmd) {
-        def encapsulatedCommand = cmd.encapsulatedCommand([0x20: 1, 0x25: 1, 0x30: 1, 0x31: 5, 0x80: 1, 0x84: 1, 0x71: 3, 0x9C: 1]) // can specify command class versions here like in zwave.parse
+		log.debug("Attempting to decrypt ${cmd?.inspect()}")
+        def encapsulatedCommand = cmd.encapsulatedCommand([0x20: 1, 0x25: 1, 0x30: 1, 0x31: 5, 0x80: 1, 0x84: 2, 0x71: 3, 0x9C: 1]) // can specify command class versions here like in zwave.parse
         if (encapsulatedCommand) {
+        	log.debug("Decrypted returned ${encapsulatedCommand?.inspect()}")
                 return zwaveEvent(encapsulatedCommand)
+        }
+        else
+        {
+        	log.debug("failed to decypt ${cmd?.inspect()}")
         }
 }
 
 
 def sensorValueEvent(value) {
+	log.debug "sensorValueEvent(value:${value})"
+    
 	if (value) {
 		createEvent(name: "contact", value: "open", descriptionText: "$device.displayName is open")
 	} else {
@@ -85,22 +104,36 @@ def sensorValueEvent(value) {
 
 def zwaveEvent(physicalgraph.zwave.commands.basicv1.BasicSet cmd)
 {
+	log.debug "BasicSet(value:${cmd.value})"
+    
 	sensorValueEvent(cmd.value)
 }
 
-def zwaveEvent(physicalgraph.zwave.commands.wakeupv1.WakeUpNotification cmd)
+def zwaveEvent(physicalgraph.zwave.commands.wakeupv2.WakeUpNotification cmd)
 {
-	def result = [createEvent(descriptionText: "${device.displayName} woke up", isStateChange: false)]
+	log.debug "WakeUpNotification. Asking for battery life."
     
-    	if (!state.lastbat || (new Date().time) - state.lastbat > 12*60*60*1000) {
-		result << secure(zwave.batteryV1.batteryGet())
-		result << response("delay 1200")
-	}
-	result << secure(zwave.wakeUpV1.wakeUpNoMoreInformation())
+	def result = [createEvent(descriptionText: "${device.displayName} woke up", isStateChange: true, displayed: true)]
+        
+    result << secure(zwave.batteryV1.batteryGet())
+    
+    if (null == state.wakeUpSet)
+    {
+    	def reportIntervalSec = 720*60 
+    
+    	log.debug "Setting wake up interval to: ${reportIntervalSec}"
+    
+    	result << secure(zwave.wakeUpV2.wakeUpIntervalSet(seconds:reportIntervalSec, nodeid:zwaveHubNodeId))
+    }
+    
+    result << response("delay 6000") // This is to give the sensor enough time to return a result	before telling the sensor to turn off its reciever           
+	result << secure(zwave.wakeUpV2.wakeUpNoMoreInformation())
 	result
 }
 
 def zwaveEvent(physicalgraph.zwave.commands.batteryv1.BatteryReport cmd) {
+	log.debug "BatteryReport(${cmd?.inspect()})"
+    
 	def map = [ name: "battery", unit: "%" ]
 	if (cmd.batteryLevel == 0xFF) {
 		map.value = 1
@@ -108,31 +141,33 @@ def zwaveEvent(physicalgraph.zwave.commands.batteryv1.BatteryReport cmd) {
 		map.isStateChange = true
 	} else {
 		map.value = cmd.batteryLevel
-	}
-	state.lastbat = new Date().time
-	[createEvent(map), secure(zwave.wakeUpV1.wakeUpNoMoreInformation())]
+        map.displayed = true
+        map.isStateChange = true
+	}	
+	[createEvent(map), secure(zwave.wakeUpV2.wakeUpNoMoreInformation())]
 }
 
-def zwaveEvent(physicalgraph.zwave.commands.manufacturerspecificv2.ManufacturerSpecificReport cmd) {
-	def result = []
-
-	def msr = String.format("%04X-%04X-%04X", cmd.manufacturerId, cmd.productTypeId, cmd.productId)
-	log.debug "msr: $msr"
-	updateDataValue("MSR", msr)
-
-	result << createEvent(descriptionText: "$device.displayName MSR: $msr", isStateChange: false)
-
-	if (!device.currentState("battery")) {
-		result << secure(zwave.batteryV1.batteryGet())
-	}
-
-	result
+def zwaveEvent(physicalgraph.zwave.commands.manufacturerspecificv2.ManufacturerSpecificReport cmd) {	    
+    log.debug "ManufacturerSpecificReport: ${cmd.inspect()}"
+    
+    def reportIntervalSec = 720*60 
+    def result = []    
+    
+    log.debug "Setting wake up interval to: ${reportIntervalSec}"
+    
+    result << secure(zwave.wakeUpV2.wakeUpIntervalSet(seconds:reportIntervalSec, nodeid:zwaveHubNodeId));
+    result << secure(zwave.batteryV1.batteryGet())
+    
+    state.wakeUpSet = true
+    
+    return result
 }
 
 def zwaveEvent(physicalgraph.zwave.Command cmd) {
-	createEvent(descriptionText: "$device.displayName: $cmd", displayed: false)
+	createEvent(descriptionText: "No handler for command: $device.displayName: ${cmd.inspect()}", displayed: state.display)
 }
 
 private secure(physicalgraph.zwave.Command cmd) {
+	log.debug "Securing command: ${cmd?.inspect()}"
 	response(zwave.securityV1.securityMessageEncapsulation().encapsulate(cmd).format())
 }
